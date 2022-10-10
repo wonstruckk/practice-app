498758d975ce5ff252d0db1a4453bfce2926b19e
reality023 <reality023@naver.com>
2022-10-10 15:29:13 +0900

    깃 세이브

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import { useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addData } from "./redux/modules/data";
+import { useEffect } from "react";
 
 function App() {
-	const dispatch = useDispatch();
-	const data = useSelector((state) => state.data);
-	const input1 = useRef(null);
-	const input2 = useRef(null);
-
-	const onClick = () => {
-		dispatch(addData(input1.current.value, input2.current.value));
-		input1.current.value = "";
-		input2.current.value = "";
-	};
+	useEffect(() => {}, []);
 
 	return (
 		<div>
-			<input type="text" ref={input1} />
-			<input type="text" ref={input2} />
-			<button onClick={onClick}>버튼</button>
-			{data.list.map((item, idx) => {
-				return (
-					<div key={idx}>
-						<h1>{item.title}</h1>
-						<p>{item.content}</p>
-					</div>
-				);
-			})}
+			<h1>안녕</h1>
+			<button>버튼2</button>
 		</div>
 	);
 }
