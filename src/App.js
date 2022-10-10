4045dcee902ea4a4dc7015db56d64dafea904c32
reality023 <reality023@naver.com>
2022-10-10 15:30:14 +0900

    styled-components Theme Provider 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
 
 function App() {
 	useEffect(() => {}, []);
 
 	return (
-		<div>
-			<h1>안녕</h1>
-			<button>버튼2</button>
-		</div>
+		<ThemeProvider
+			theme={{
+				color: {
+					red: "#ff0000",
+				},
+			}}
+		>
+			<div>
+				<h1>안녕</h1>
+				<button>버튼2</button>
+			</div>
+		</ThemeProvider>
 	);
 }
 
